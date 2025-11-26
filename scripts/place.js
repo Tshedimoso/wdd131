ecec6c708c5891892f64ed0688bc20da65c43146
Tshedimoso <tsegona@byupathway.edu>
2025-11-26 16:10:35 +0200

    footer

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+const yearEl = document.getElementById('currentyear');
+const lastModEl = document.getElementById('lastModified');
+if (yearEl) yearEl.textContent = new Date().getFullYear();
+if (lastModEl) lastModEl.textContent = document.lastModified || 'Unknown';})
